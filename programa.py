77691f29036ab18ce46970f65dfba4bf91b3b775
Moises Ramirez <gemearygatito17@gmail.com>
2026-07-15 10:51:22 -0400

    Agregada opcion 4 y pulido de opciones pasadas

--- programa.py
+++ programa.py
@@ -29,6 +29,7 @@ def menu():
     print("4. Agregar prenda")
     print("5. Eliminar prenda")
     print("6. Salir")
+    print("=====================================")
 
 def leer_opc():
     while True:
@@ -47,10 +48,10 @@ def unidades_cat(categoria, prendas, bodega):
     unidades=0
     #bucle for para encontrar claves con esa categoria
     for clave, valor in prendas.items():
-        if valor[1]==categoria:
+        if valor[1].lower().strip()==categoria.lower().strip():
             unidades+=bodega[clave][1]
     
-    print(f"Hay {unidades} unidades de la categoria {categoria}")
+    print(f"El total de unidades disponibles es: {unidades}")
 
 #Opcion 2
 def busqueda_precio(p_min, p_max, prendas, bodega):
@@ -65,7 +66,7 @@ def busqueda_precio(p_min, p_max, prendas, bodega):
     #Ordenar y mostrar si tiene contenido
     else:
         lista.sort()
-        print(lista)
+        print(f"Las prendas encontradas son: {lista}")
 
 #Opcion 3
 def buscar_cod(codigo, bodega):
@@ -79,6 +80,50 @@ def actualizar_precio(codigo, p_nuevo, bodega):
         return True
     return False
 
+#Opcion 4
+
+#Validaciones
+def validar_texto(texto):
+    if texto.strip()=="":
+        return False
+    return True
+
+def validar_unisex(es_unisex):
+    if not es_unisex in("s", "n"):
+        return False
+    return True
+
+def validar_precio(precio):
+    if precio>0:
+        return True
+    return False
+
+def validar_unidades(unidades):
+    if unidades>=0:
+        return True
+    return False
+
+#Agregar la prenda
+def agregar_prenda(codigo, nombre, categoria, talla, color, material, es_unisex, precio, unidades, prendas, bodega):
+    #Primero verificar que el codigo no este registrado
+    if codigo in prendas:
+        return False
+    else:
+        #Pasar el es_unisex a booleano
+        if es_unisex=="s":
+            es_unisex=True
+        elif es_unisex=="n":
+            es_unisex=False
+
+        #Asignar los valores para cada diccionario
+        valor_prendas=[nombre, categoria, talla, color, material, es_unisex]
+        valor_bodega=[precio, unidades]
+
+        #Agregar prenda a los diccionarios
+
+        prendas[codigo]=valor_prendas
+        bodega[codigo]=valor_bodega
+        return True
 
 #Programa principal
 def main():
@@ -110,7 +155,7 @@ def main():
             while True:
                 codigo=input("Ingrese el codigo de la prenda a actualizar: ").upper().strip()
                 try:
-                    p_nuevo=int(input("Ingrese el nuevo precio"))
+                    p_nuevo=int(input("Ingrese el nuevo precio: "))
                     if p_nuevo<=0:
                         print("El precio nuevo debe ser un valor entero positivo")
                         continue
@@ -122,11 +167,70 @@ def main():
                 else:
                     print("El codigo no existe")
                 
-                opc=input("¿Desea actualizar otro precio (s/n)?").lower().strip()
+                opc=input("¿Desea actualizar otro precio (s/n)?: ").lower().strip()
                 if opc=="s":
                     continue
                 else:
                     break    
+        elif opc==4:
+            while True:
+                #Solicitud y validacion de variables de texto basicas
+                codigo=input("Ingrese el codigo: ").upper()
+                if not validar_texto(codigo):
+                    print("El codigo no deber ser vacio ni solo espacios en blanco")
+                    break
+                nombre=input("Ingrese el nombre: ").capitalize()
+                if not validar_texto(nombre):
+                    print("El nombre no debe ser vacio ni solo espacios en blanco")
+                    break
+                categoria=input("Ingrese la categoria: ").lower()
+                if not validar_texto(categoria):
+                    print("La categoria no debe ser vacia ni solo espacios en blanco")
+                    break
+                talla=input("Ingrese la talla: ").upper()
+                if not validar_texto(talla):
+                    print("La talla no debe ser sre vacia ni solo espacios en blanco")
+                    break
+                color=input("Ingrese el color: ").lower()
+                if not validar_texto(color):
+                    print("El color no debe ser vacio ni solo espacios en blanco")
+                    break
+                material=input("Ingrese el material: ").lower()
+                if not validar_texto(material):
+                    print("El material no debe ser vacio ni solo espacios en blanco")
+                    break
+                #Validacion de unisex
+                es_unisex=input("Ingrese si es unisex (s/n): ").lower()
+                if not validar_unisex(es_unisex):
+                    print("Debe ingresar s/n")
+                    break
+                #Validacion de numeros
+                try:
+                    precio=int(input("Ingrese el precio: "))
+                    if not validar_precio(precio):
+                        print("El precio debe ser un numero entero mayor a cero")
+                        break
+                    unidades=int(input("Ingrese las unidades: "))
+                    if not validar_unidades(unidades):
+                        print("Las unidades deben ser un entero mayor o igual a cero")
+                        break
+                except ValueError:
+                    print("Se debe ingresar un numero entero")
+                    break
+                
+                #Si todo ha sido validado agregar la prenda
+                if agregar_prenda(codigo, nombre, categoria, talla, color, material, es_unisex, precio, unidades, prendas, bodega):
+                    print("Prenda agregada")
+                else:
+                    print("El codigo ya existe")
+                break
 
 
+        elif opc==5:
+            print("nada")
+
+        elif opc==6:
+            print("Programa finalizado.")
+            time.sleep(3)
+            break
 main()
